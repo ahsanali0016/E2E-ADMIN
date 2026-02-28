62a86250189e4e7491ebaabf0e24a206044b0f8f
ahsanali0016 <attituders789@gmail.com>
2026-02-28 08:54:48 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -906,5 +906,6 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
+
 
